df5b7795e00ec9cd7e41256a0a8687bdce4c992f
App Academy Student <student@appacademy.io>
2016-07-14 11:25:16 -0700

    beat level-8

--- board.rb
+++ board.rb
@@ -53,9 +53,9 @@ class Board
 
   def solved?
     rows.all? { |row| solved_set?(row) } &&
-      columns.all? { |col| solved_set?(col) } &&
-      squares.all? { |square| solved_set?(square) }
-    end
+    columns.all? { |col| solved_set?(col) } &&
+    squares.all? { |square| solved_set?(square) }
+    # end
   end
 
   def solved_set?(tiles)
