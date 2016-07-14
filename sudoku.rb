719f35293603d8b3fa1fb1307d34f91ddc2f5645
App Academy Student <student@appacademy.io>
2016-07-14 14:10:05 -0700

    beat super-evil-level

--- sudoku.rb
+++ sudoku.rb
@@ -5,12 +5,12 @@ puts "Only contractors write code this bad.".yellow
 
 class SudokuGame
   def self.from_file(filename)
-    board = self.from_file(filename)
+    board = Board.from_file(filename)
     self.new(board)
   end
 
   def initialize(board)
-    @board = [[]]
+    @board = board
   end
 
   def method_missing(method_name, *args)
@@ -31,9 +31,9 @@ class SudokuGame
       begin
         pos = parse_pos(gets)
       rescue
-        TODO: Google how to print the error that happened inside of a rescue statement.
-        puts "Invalid position entered (did you use a comma?)"
-        puts ""
+        # : Google how to print the error that happened inside of a rescue statement.
+        print "Invalid position entered (did you use a comma?)"
+        print "\n\n"
 
         pos = nil
       end
@@ -41,12 +41,20 @@ class SudokuGame
     pos
   end
 
+  def parse_val(val)
+    Integer(val)
+  end
+
+  def parse_pos(pos)
+    pos.split(",").map { |el| Integer(el) }
+  end
+
   def get_val
     val = nil
     until val && valid_val?(val)
       puts "Please enter a value between 1 and 9 (0 to clear the tile)"
       print "> "
-      val = parse_val(gets)
+      val = parse_val(gets.chomp)
     end
     val
   end
@@ -55,7 +63,7 @@ class SudokuGame
     board.render
     pos = get_pos
     val = get_val
-    board[*pos] = val
+    board[pos] = val
   end
 
   def run
@@ -65,16 +73,17 @@ class SudokuGame
   end
 
   def solved?
-    self.solved?
+    board.solved?
   end
 
   def valid_pos?(pos)
-    if pos.is_a?(:Array) &&
-      pos.length = 2 &&
-      pos.all? { |x| x.in?(0, board.size - 1) }
+    if pos.is_a?(Array) &&
+      pos.length == 2 &&
+      pos.all? { |x| x < 9 && x > -1 }
       return true
     else
       get_pos
+    end
   end
 
   def valid_val?(val)
@@ -88,3 +97,4 @@ end
 
 
 game = SudokuGame.from_file("puzzles/sudoku1.txt")
+game.run
